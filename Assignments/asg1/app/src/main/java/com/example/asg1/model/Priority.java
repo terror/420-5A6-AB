8f1000f1d50966333d03374ca6d4086f8b053e63
Liam <liam@scalzulli.com>
2021-09-07 13:58:34 -0400

    [A1] Set up initial structure

---
+++ Assignments/asg1/app/src/main/java/com/example/asg1/model/Priority.java
@@ -0,0 +1,11 @@
+package com.example.asg1.model;
+
+/**
+ * Task priority values.
+ */
+public enum Priority {
+  NONE,
+  LOW,
+  MEDIUM,
+  HIGH;
+}
---
+++ Assignments/asg1/app/src/main/java/com/example/asg1/model/Status.java
@@ -0,0 +1,13 @@
+package com.example.asg1.model;
+
+/**
+ * Task status values.
+ */
+public enum Status {
+  PENDING,
+  DELETED,
+  COMPLETED,
+  WAITING,
+  RECURRING,
+  NONE;
+}
